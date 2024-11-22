2902626cfb752fcb5cc8a178bc27ced58e5bde0c
Mohsin <mohsinrajpoot793605@gmail.com>
2024-11-22 19:05:57 +0500

    dee

--- src/App.js
+++ src/App.js
@@ -1,27 +1,31 @@
 import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "../src/Pages/LandingPage/LandingPage";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SavedArticles from "./Pages/SavedArticls/SavedNewsPage";
-import { getFromLocalStorage } from "./utils/ThirdPartyApi"; 
+import { getFromLocalStorage } from "./utils/ThirdPartyApi";
 
 function App() {
   const [savedArticles, setSavedArticles] = useState([]);
 
   useEffect(() => {
-    const storedArticles = getFromLocalStorage('newsData');
-    if (storedArticles) {
-      setSavedArticles(storedArticles); 
+    try {
+      const storedArticles = getFromLocalStorage("newsData");
+      if (storedArticles) {
+        setSavedArticles(storedArticles);
+      }
+    } catch (error) {
+      console.error("Error reading from local storage:", error);
     }
   }, []);
 
   return (
     <div className="App">
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <Routes>
           <Route path="/" element={<LandingPage />} />
-          <Route 
-            path="/saved-articles" 
-            element={<SavedArticles savedArticles={savedArticles} />} 
+          <Route
+            path="/saved-articles"
+            element={<SavedArticles savedArticles={savedArticles} />}
           />
         </Routes>
       </Router>
